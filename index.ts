c798a0ea9c4bc36d15c7ac35481df7ed1496383b
bespalovnv <bespalovnv@rus-dev.com>
2020-01-14 13:54:07 +0500

    механизм определения типов

---
+++ index.ts
@@ -0,0 +1,9 @@
+let anyTypeVariable;
+anyTypeVariable = 10;
+anyTypeVariable = '';
+
+
+let declaredVariableWithNumberType: number;
+declaredVariableWithNumberType = 10;
+// declaredVariableWithNumberType = ''; => Error: Assigned expression type "" is not assignable to type number
+
